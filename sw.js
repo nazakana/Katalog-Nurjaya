4a46c0844b97a7286dc72a3a6becc932b9383eef
Nazakana <156926231+nazakana@users.noreply.github.com>
2026-08-05 10:19:17 +0700

    Master file of Katalog Nurjaya
    Katalog Standalone V1

---
+++ sw.js
@@ -0,0 +1,13 @@
+// Service Worker Minimalis untuk PWA
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  // Meloloskan request jaringan biasa
+  e.respondWith(fetch(e.request));
+});
